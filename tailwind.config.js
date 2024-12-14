09a4cf05db7142fd444eadaa635c5298291eb3e8
HyunJinNo <nhj9831@naver.com>
2024-12-14 14:39:46 +0900

    design: 커스텀 스타일 선언

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#26B888',
+        'primary-red': '#DA1E28',
+        gray: '#F5F5F5',
+        green: '#ECF4E2',
+        lightGreen: '#F2F6EC',
+        lightGray: '#F8F8F8',
+      },
+    },
   },
   plugins: [],
 };
