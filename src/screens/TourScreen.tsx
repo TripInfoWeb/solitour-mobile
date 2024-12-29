d4746c7a0b1fe1f513b6b7da379b470e5def0e56
HyunJinNo <nhj9831@naver.com>
2024-12-29 14:53:01 +0900

    design: 내 여행 화면 UI 구현

--- src/screens/TourScreen.tsx
+++ src/screens/TourScreen.tsx
@@ -4,7 +4,8 @@ import {Image, Text, View} from 'react-native';
 
 export const TourScreen = () => {
   return (
-    <View style={tw`flex h-full flex-col items-center justify-center bg-white`}>
+    <View
+      style={tw`flex h-full flex-col items-center justify-center bg-white px-4`}>
       <View style={tw`flex flex-col items-center gap-[1.125rem]`}>
         <Image
           style={tw`h-16 w-16`}
@@ -12,6 +13,13 @@ export const TourScreen = () => {
         />
         <Text>아직 저장된 여행이 없어요</Text>
       </View>
+      {/* TODO
+      <FlatList
+        contentContainerStyle={tw`flex flex-col gap-4`}
+        data={[1, 2, 3, 4, 5, 6]}
+        renderItem={() => <TourItem />}
+        keyExtractor={item => item.toString()}
+      /> */}
     </View>
   );
 };
