b4274a58649ab05663a2cd78c0c84d89efecf2f6
HyunJinNo <nhj9831@naver.com>
2024-12-29 23:27:14 +0900

    feat: 여행일기 작성 시 등록한 이미지 취소 기능 구현

--- src/screens/diary/DiaryEditorScreen.tsx
+++ src/screens/diary/DiaryEditorScreen.tsx
@@ -4,9 +4,11 @@ import {
   Image,
   KeyboardAvoidingView,
   Platform,
+  Pressable,
   SafeAreaView,
   ScrollView,
   TextInput,
+  View,
 } from 'react-native';
 import {tw} from '@src/libs/tailwind';
 import {DiaryDatePicker} from '@src/components/diary/editor/date/DiaryDatePicker';
@@ -71,8 +73,24 @@ export const DiaryEditorScreen = () => {
           contentContainerStyle={tw`gap-2`}
           horizontal={true}
           data={images}
-          renderItem={({item}) => (
-            <Image style={tw`h-20 w-20 rounded-lg`} source={{uri: item}} />
+          renderItem={({item, index}) => (
+            <Pressable
+              style={tw`relative`}
+              onPress={() =>
+                setImages(images.filter((_, idx) => idx !== index))
+              }>
+              <Image
+                style={tw`h-20 w-20 rounded-lg border border-custom-04`}
+                source={{uri: item}}
+              />
+              <View
+                style={tw`absolute right-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-primary-green`}>
+                <Image
+                  style={tw`h-2.5 w-2.5`}
+                  source={require('@src/assets/common/close-white.png')}
+                />
+              </View>
+            </Pressable>
           )}
           keyExtractor={(_, index) => index.toString()}
         />
