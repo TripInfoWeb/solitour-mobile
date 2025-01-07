670e427f6eef48fc4e65b4e6bd12362c134805eb
HyunJinNo <nhj9831@naver.com>
2025-01-07 19:32:54 +0900

    chore: @tanstack/eslint-plugin-query 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:@tanstack/query/recommended'],
   rules: {
     '@typescript-eslint/func-call-spacing': 'off',
   },
