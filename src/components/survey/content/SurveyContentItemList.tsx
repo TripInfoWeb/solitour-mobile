7ddb966aeb58d39b0803db968e576f58b5e12806
HyunJinNo <nhj9831@naver.com>
2025-01-17 22:29:08 +0900

    refactor: useSurveyContentItemList 훅 생성

--- src/components/survey/content/SurveyContentItemList.tsx
+++ src/components/survey/content/SurveyContentItemList.tsx
@@ -3,41 +3,17 @@ import React from 'react';
 import {FlatList} from 'react-native';
 import {SurveyContentItem} from './SurveyContentItem';
 import {useSurveyStore} from '@src/stores/surveyStore';
-import {useSuspenseQuery} from '@tanstack/react-query';
-import EncryptedStorage from 'react-native-encrypted-storage';
-import {BACKEND_URL} from '@env';
-import {SurveyContentList} from '@src/types/survey';
+import {useSurveyContentItemList} from '@src/hooks/survey/content/useSurveyContentItemList';
 
 export const SurveyContentItemList = () => {
   const {contentCategory, contentTitles, setSurveyState} = useSurveyStore();
-  const {data} = useSuspenseQuery<SurveyContentList>({
-    queryKey: ['surveyContentItemList', contentCategory],
-    queryFn: async () => {
-      const accessToken = await EncryptedStorage.getItem('access_token');
-      const response = await fetch(
-        `${BACKEND_URL}/api/media?type=${contentCategory}&page=0&size=8`,
-        {
-          method: 'GET',
-          headers: {Cookie: `access_token=${accessToken}`},
-        },
-      );
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch data.');
-      }
-
-      return await response.json();
-    },
-    staleTime: 600000,
-    gcTime: 1800000,
-    retry: false,
-  });
+  const {surveyContentList} = useSurveyContentItemList(contentCategory!);
 
   return (
     <FlatList
       contentContainerStyle={tw`gap-[1.125rem] pb-20`}
       columnWrapperStyle={tw`gap-2.5`}
-      data={data.content}
+      data={surveyContentList.content}
       renderItem={({item}) => (
         <SurveyContentItem
           title={item.mediaName}
