3b5e37457847dd3b98b85ddefa9dfe5969e96ffa
HyunJinNo <nhj9831@naver.com>
2025-02-15 19:51:10 +0900

    design: SurveyContentItemListSkeleton 컴포넌트 수정

--- src/components/skeleton/survey/content/SurveyContentItemListSkeleton.tsx
+++ src/components/skeleton/survey/content/SurveyContentItemListSkeleton.tsx
@@ -1,17 +1,27 @@
 import {tw} from '@src/libs/tailwind';
 import React from 'react';
-import {FlatList} from 'react-native';
+import {Animated, FlatList, View} from 'react-native';
 import {SurveyContentItemSkeleton} from './SurveyContentItemSkeleton';
+import {usePulseAnimation} from '@src/hooks/common/usePulseAnimation';
 
 export const SurveyContentItemListSkeleton = () => {
+  const {opacity} = usePulseAnimation();
+
   return (
-    <FlatList
-      contentContainerStyle={tw`gap-[1.125rem] pb-20`}
-      columnWrapperStyle={tw`gap-2.5`}
-      data={[0, 1, 2, 3]}
-      renderItem={() => <SurveyContentItemSkeleton />}
-      keyExtractor={item => item.toString()}
-      numColumns={2}
-    />
+    <View>
+      <Animated.View
+        style={tw.style('mb-4 h-[3.25rem] rounded-full bg-custom-gray', {
+          opacity,
+        })}
+      />
+      <FlatList
+        contentContainerStyle={tw`gap-[1.125rem] pb-20`}
+        columnWrapperStyle={tw`gap-2.5`}
+        data={[0, 1, 2, 3]}
+        renderItem={() => <SurveyContentItemSkeleton />}
+        keyExtractor={item => item.toString()}
+        numColumns={2}
+      />
+    </View>
   );
 };
