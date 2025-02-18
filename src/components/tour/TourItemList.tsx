372b3564320074d96c8e70e62fa425f4d1833b71
HyunJinNo <nhj9831@naver.com>
2025-02-18 17:36:55 +0900

    design: 여행 계획이 없을 때 표시할 UI 추가

--- src/components/tour/TourItemList.tsx
+++ src/components/tour/TourItemList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {Image, ScrollView, Text, View} from 'react-native';
 import {tw} from '@src/libs/tailwind';
 import {TourItem} from './TourItem';
 import {useTourItemList} from '@src/hooks/tour/useTourItemList';
@@ -13,9 +13,19 @@ export const TourItemList = () => {
         tourItemList.length === 0 ? 'bg-white' : 'bg-[#F3F3F3]',
         'flex h-full flex-col px-4',
       )}>
-      {tourItemList.map(item => (
-        <TourItem key={item.plan.planId} data={item} />
-      ))}
+      {tourItemList.length === 0 ? (
+        <View style={tw`flex flex-col items-center gap-[1.125rem]`}>
+          <Image
+            style={tw`h-16 w-16`}
+            source={require('@src/assets/tour/tour-empty.png')}
+          />
+          <Text>아직 저장된 여행이 없어요</Text>
+        </View>
+      ) : (
+        tourItemList.map(item => (
+          <TourItem key={item.plan.planId} data={item} />
+        ))
+      )}
     </ScrollView>
   );
 };
